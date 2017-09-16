3e2d822bf8a62ad0fc04c145f3448701a95448cd
Karel Ha <mathemage@gmail.com>
2017-09-16 11:59:37 +0200

    Enhance filtering in lookup_city()

--- cityprofile/lookupcity/lookup.py
+++ cityprofile/lookupcity/lookup.py
@@ -7,14 +7,36 @@ from cityprofile.csvtojson.csvtojson import read_csv_file, get_pretty_json_strin
 RESOURCES_DIR = "../resources"
 
 
+def check_with_misc_filters(csv_row):
+    # print("csv_row['uzemiz_cis'] == {}".format(csv_row['uzemiz_cis']))
+    return csv_row['uzemiz_cis'] == ''
+
+
+def lookup_city_in_dataset(city_name, csv_content):
+    x_column_name = 'rok'
+    y_column_name = 'hodnota'
+    city_column_name = 'uzemi_txt'
+    chart_names = ['Počet turistů', "Počet přenocování turistů"]
+    chart_names_column_name = 'stapro_txt'
+    chart_xy_data = []
+    for chart_name in chart_names:
+        filtered_content = [{'x': csv_row[x_column_name], 'y': csv_row[y_column_name]}
+                            for csv_row in csv_content
+                            if csv_row[city_column_name] == city_name
+                            and csv_row[chart_names_column_name] == chart_name
+                            and check_with_misc_filters(csv_row)]
+        chart_xy_data.append({chart_name: filtered_content})
+    return chart_xy_data
+
+
 def lookup_city(city_name):
-    print("Looking up the city of {}...".format(city_name))
+    # print("Looking up the city of {}...".format(city_name))
     datasets = [csv_file for csv_file in os.listdir(RESOURCES_DIR) if csv_file.endswith(".csv")]
 
     for dataset in datasets:
         filepath = "{}/{}".format(RESOURCES_DIR, dataset)
         print('Exploring "{}" ...'.format(filepath))
         csv_content = read_csv_file(filepath)
-        filtered_content = [csv_row for csv_row in csv_content if csv_row['uzemi_txt'] == city_name]
-        json_of_dataset = {dataset: filtered_content}
+        chart_xy_data = lookup_city_in_dataset(city_name, csv_content)
+        json_of_dataset = {dataset: chart_xy_data}
         print(get_pretty_json_string(json_of_dataset))
